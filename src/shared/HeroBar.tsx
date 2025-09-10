40b14f79e8a50b9d4022420a7fdd9773f07800d5
Yan <yanwang5@andrew.cmu.edu>
2025-09-10 15:38:08 -0400

    Update HeroBar.tsx

--- src/shared/HeroBar.tsx
+++ src/shared/HeroBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HeroBar({ src, alt, children, className = '' }: { src: string, alt: string, children?: React.ReactNode, className?: string }) {
   return (
     <section className="w-full">
-      <div className={`relative w-full`}>
+      <div className={`relative w-full mb-12`}>
         <img src={src} alt={alt} className={`w-full object-cover ${className}`} style={{ aspectRatio: '1440 / 252' }} />
         {children ? <div className="absolute inset-0">{children}</div> : null}
       </div>
