b51f7a4822456b4d9adae19b90ad5b59401997cc
Yan <yanwang5@andrew.cmu.edu>
2025-09-10 16:10:01 -0400

    Update Books.tsx

--- src/routes/Books.tsx
+++ src/routes/Books.tsx
@@ -4,26 +4,11 @@ export default function Books() {
   return (
     <div>
       <HeroBar src="/assets/Books & Journals Header.png" alt="Books & Journals" />
+      
       <section className="container-xxl pt-10 pb-16">
         <h1 className="text-3xl md:text-4xl font-semibold tracking-tight">Recommended Books & Journals</h1>
-        <p className="mt-3 text-sr-muted">15 slots planned.
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          .
-          
-        </p>
+        <h2 className="h2">Overview</h2>
+        <p className="p text-gray-600">15 slots planned.</p>
       </section>
     </div>
   )
