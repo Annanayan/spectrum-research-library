314c851db640a2f35ef9f5f436143b604f74d5ef
Yan <yanwang5@andrew.cmu.edu>
2025-09-13 15:23:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
-  server: { port: 5173 },
-  preview: { port: 5173 }
+  server: { port: 5173, compression: true},
+  preview: { port: 5173 },
+  build: {
+    // 分割代码
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+    // 压缩
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 })
